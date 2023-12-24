751653151f2f7f97ce3d21cd07cf9cf7c7a38b3a
余童捷 <202100202045@stumail.sztu.edu.cn>
2023-12-24 23:00:31 +0800

    修改: 添加Signin实体类 （未配置mapper）

---
+++ RainngCourseBE/src/main/java/com/rainng/coursesystem/model/entity/SigninEntity.java
@@ -0,0 +1,54 @@
+package com.rainng.coursesystem.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.NonNull;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.NotNull;
+
+@TableName("signin_record")
+@Data
+public class SigninEntity {
+    private static final String ID = "signin_id";
+    private static final String LOCATION = "location";
+    private static final String TIMESTAMP = "timestamp";
+    private static final String STATUS = "status";
+    private static final String STUDENT = "signin_student";
+    private static final String COURSE = "signin_course";
+    /**
+     * 签到状态
+     */
+    public enum SigninStatus {
+        SUCCESS,     // 签到成功
+        FAILED, // 未签到
+        ERROR        // 错误
+    }
+    @NonNull
+    @TableId(value = ID, type = IdType.AUTO)
+    private Long id;
+
+    @NotNull(message = "必须选择签到时间")
+    @TableField(TIMESTAMP)
+    private Data timeStamp;
+
+    @NotNull(message = "必须选择地点")
+    @TableField(LOCATION)
+    private String location;
+
+    @NotNull
+    @TableField(STATUS)
+    private SigninStatus status;
+
+    @NotNull
+    @TableField(STUDENT)
+    private String student_id;
+
+    @NotNull
+    @TableField(COURSE)
+    private String course_id;
+
+}
